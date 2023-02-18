366b0893342ee0428f69e1be5071ca15d8e41ec2
Paul Armstrong <paul@spaceyak.com>
2023-02-18 08:05:13 -0800

    feat(plugin-prettier): respect prettierignore before passing files through
    Fixes #59

---
+++ .changeset/hip-students-speak.md
@@ -0,0 +1,5 @@
+---
+'@onerepo/plugin-prettier': minor
+---
+
+Respect .prettier files when formatting across files and paths (as opposed to `--all`)
--- plugins/prettier/src/commands/prettier.ts
+++ plugins/prettier/src/commands/prettier.ts
@@ -1,4 +1,7 @@
+import path from 'node:path';
+import { minimatch } from 'minimatch';
 import { updateIndex } from '@onerepo/git';
+import { exists, lstat, read } from '@onerepo/file';
 import { run } from '@onerepo/subprocess';
 import type { WithAllInputs } from '@onerepo/builders';
 import { withAllInputs } from '@onerepo/builders';
@@ -26,23 +29,54 @@ export const builder: Builder<Args> = (yargs) =>
 			type: 'boolean',
 		});
 
-export const handler: Handler<Args> = async function handler(argv, { getFilepaths }) {
+export const handler: Handler<Args> = async function handler(argv, { getFilepaths, graph }) {
 	const { add, all, check, 'dry-run': isDry } = argv;
 
-	const paths: Array<string> = await getFilepaths();
+	const filteredPaths = [];
+	if (!all) {
+		const ignoreStep = logger.createStep('Filtering ignored files');
+		const ignoreFile = graph.root.resolve('.prettierignore');
+		const hasIgnores = await exists(ignoreFile, { step: ignoreStep });
+		const rawIgnores = await (hasIgnores ? read(ignoreFile, 'r', { step: ignoreStep }) : '');
+		const ignores = rawIgnores.split('\n').filter((line) => Boolean(line.trim()) && !line.trim().startsWith('#'));
 
-	if (paths.length === 0) {
+		const paths = await getFilepaths();
+		for (const filepath of paths) {
+			const ext = path.extname(filepath);
+			if (!ext) {
+				const stat = await lstat(graph.root.resolve(filepath), { step: ignoreStep });
+				const isDirectory = stat && stat.isDirectory();
+				if (isDirectory) {
+					filteredPaths.push(filepath);
+				}
+				continue;
+			}
+
+			if (!ignores.some((pattern) => minimatch(filepath, pattern))) {
+				filteredPaths.push(filepath);
+			}
+		}
+
+		await ignoreStep.end();
+	}
+
+	if (!all && !filteredPaths.length) {
 		logger.warn('No filepaths to check for formatting');
 		return;
 	}
 
 	await run({
-		name: `Format files ${all ? '' : paths.join(', ').substring(0, 60)}…`,
+		name: `Format files ${all ? '' : filteredPaths.join(', ').substring(0, 60)}…`,
 		cmd: 'npx',
-		args: ['prettier', '--ignore-unknown', isDry || check ? '--list-different' : '--write', ...paths],
+		args: [
+			'prettier',
+			'--ignore-unknown',
+			isDry || check ? '--list-different' : '--write',
+			...(all ? ['.'] : filteredPaths),
+		],
 	});
 
-	if (add) {
-		await updateIndex(paths);
+	if (add && filteredPaths.length) {
+		await updateIndex(filteredPaths);
 	}
 };
