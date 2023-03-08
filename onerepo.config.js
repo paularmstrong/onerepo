97ac7ec901f82304af2ef520b49c04838aad5204
Paul Armstrong <paul@spaceyak.com>
2023-03-07 17:50:21 -0800

    docs: remove full CLI docs from contributing section

--- onerepo.config.js
+++ onerepo.config.js
@@ -2,10 +2,7 @@
 export default {
 	'pre-commit': {
 		sequential: [{ match: '**/*.{ts,tsx,js,jsx,astro}', cmd: '$0 lint --add' }, '$0 format --add', '$0 tsc'],
-		parallel: [
-			{ match: '**/commands/**/*.ts', cmd: '$0 docgen --add' },
-			{ match: '**/package.json', cmd: '$0 graph verify' },
-		],
+		parallel: [{ match: '**/package.json', cmd: '$0 graph verify' }],
 	},
 	'pre-merge': {
 		sequential: ['$0 lint --all --no-fix', '$0 format --check', '$0 test --affected', '$0 tsc', '$0 build'],
