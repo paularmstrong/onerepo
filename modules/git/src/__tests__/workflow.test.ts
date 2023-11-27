0c2af3fe74f84cd051744aae7da0cec4e9c4f291
Paul Armstrong <paul@spaceyak.com>
2023-11-27 09:17:05 -0800

    test: stub in workflow test

---
+++ modules/git/src/__tests__/workflow.test.ts
@@ -0,0 +1,33 @@
+import * as subprocess from '@onerepo/subprocess';
+import * as file from '@onerepo/file';
+import { getLogger } from '@onerepo/logger';
+import { getGraph } from '@onerepo/graph';
+import { StagingWorkflow } from '../workflow';
+
+const graph = getGraph(__dirname);
+
+describe('StagingWorkflow', () => {
+	beforeEach(() => {
+		vi.spyOn(subprocess, 'run').mockResolvedValue(['', '']);
+		vi.spyOn(file, 'exists').mockResolvedValue(false);
+		vi.spyOn(file, 'read').mockResolvedValue('');
+		vi.spyOn(file, 'remove').mockResolvedValue(undefined);
+		vi.spyOn(file, 'write').mockResolvedValue(undefined);
+	});
+
+	describe('saveUnstaged', () => {
+		test('checks status', async () => {
+			const logger = getLogger({ verbosity: 0 });
+			const workflow = new StagingWorkflow({ graph, logger });
+
+			await workflow.saveUnstaged();
+
+			expect(subprocess.run).toHaveBeenCalledWith(
+				expect.objectContaining({
+					cmd: 'git',
+					args: ['status', '-z'],
+				}),
+			);
+		});
+	});
+});
