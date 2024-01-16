ed776cd72c318a2e931a7015ccc3132bf21b540c
Paul Armstrong <paul@spaceyak.com>
2024-01-16 15:43:51 -0800

    docs: sync tabs

--- docs/src/components/modules/render-markdown.ts
+++
@@ -1,9 +0,0 @@
-/**
- * This needs to be pulled out into a TS file in order for eslint to parse from markdown-remark.
- */
-import { createMarkdownProcessor } from '@astrojs/markdown-remark';
-
-export async function getMarkdownRenderer() {
-	const { render } = await createMarkdownProcessor({});
-	return render;
-}
