bdac6622f4d859d45570d127b2eb799f9aaecba8
Paul Armstrong <me@paularmstrong.dev>
2024-02-05 11:29:53 -0800

    ci: require change entries

--- onerepo.config.ts
+++ onerepo.config.ts
@@ -46,17 +46,19 @@ export default {
 
 	tasks: {
 		'pre-commit': {
-			serial: [['$0 lint --staged --add', '$0 format --staged --add'], '$0 tsc --staged'],
-			parallel: [
-				{ match: '**/package.json', cmd: '$0 graph verify' },
+			serial: [
 				{ match: '**/onerepo.config.*', cmd: ['$0 codeowners sync --add'] },
+				['$0 lint --staged --add', '$0 format --staged --add'],
+				'$0 tsc --staged',
 			],
+			parallel: [{ match: '**/package.json', cmd: '$0 graph verify' }, '$0 change verify'],
 		},
 		'pre-merge': {
 			serial: [['$0 lint --all --no-fix', '$0 format --check'], '$0 test -a', '$0 jest -a', '$0 tsc', '$0 build'],
 			parallel: [
 				{ match: '**/package.json', cmd: '$0 graph verify' },
 				{ match: '**/onerepo.config.*', cmd: ['$0 codeowners verify'] },
+				'$0 change verify',
 			],
 		},
 		build: {
