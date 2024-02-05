59a97ff531ee26bfa00b8e0f46a9ff3233e06d57
Paul Armstrong <me@paularmstrong.dev>
2024-02-04 18:18:13 -0800

    feat(changes): add 'verify' command
    Fixes #537

--- modules/onerepo/src/core/changes/index.ts
+++ modules/onerepo/src/core/changes/index.ts
@@ -3,6 +3,7 @@ import * as Add from './add';
 import * as Migrate from './migrate';
 import * as Publish from './publish';
 import * as Snapshot from './snapshot';
+import * as Verify from './verify';
 import * as Version from './version';
 
 export const changes: Plugin = function codeowners(config) {
@@ -13,6 +14,7 @@ export const changes: Plugin = function codeowners(config) {
 			const migrate = visitor(Migrate);
 			const publish = visitor(Publish);
 			const snapshot = visitor(Snapshot);
+			const verify = visitor(Verify);
 			const version = visitor(Version);
 
 			return yargs.command(
@@ -39,6 +41,7 @@ export const changes: Plugin = function codeowners(config) {
 						)
 						.command(publish.command, publish.description, publish.builder, publish.handler)
 						.command(snapshot.command, snapshot.description, snapshot.builder, snapshot.handler)
+						.command(verify.command, verify.description, verify.builder, verify.handler)
 						.command(version.command, version.description, version.builder, version.handler)
 						.demandCommand(1);
 
