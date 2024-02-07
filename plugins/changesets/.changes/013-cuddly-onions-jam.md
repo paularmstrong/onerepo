ea70c4570f6594f44a09f70c98db8151ebcff94c
Paul Armstrong <me@paularmstrong.dev>
2024-02-07 08:44:11 -0800

    feat: deprecation notice in plugin-changesets

---
+++ plugins/changesets/.changes/013-cuddly-onions-jam.md
@@ -0,0 +1,5 @@
+---
+type: minor
+---
+
+A deprecation notice will show for every command in this plugin. Switch to the built-in change management: https://onerepo.tools/core/changes/
--- plugins/changesets/src/index.ts
+++ plugins/changesets/src/index.ts
@@ -1,3 +1,4 @@
+import { getLogger } from '@onerepo/logger';
 import type { Plugin } from 'onerepo';
 import * as commands from './commands';
 
@@ -43,12 +44,26 @@ export function changesets(opts: Options = {}): Plugin {
 					yargs.usage(`$0 ${Array.isArray(name) ? name[0] : name} <command> [options...]`).demandCommand(1);
 
 					for (const cmd of resolved) {
-						yargs.command(cmd.command, cmd.description, cmd.builder, cmd.handler);
+						yargs.command(cmd.command, cmd.description, cmd.builder, cmd.handler, [middleware], deprecationNotice);
 					}
 					return yargs;
 				},
 				() => {},
+				[middleware],
+				deprecationNotice,
 			);
 		},
 	};
 }
+
+function middleware() {
+	const logger = getLogger();
+	const v = logger.verbosity;
+	logger.verbosity = 2;
+	const bar = '⎯'.repeat(Math.min(process.stdout.columns, 100));
+	logger.warn(`${bar}\n\n${deprecationNotice}\n\n${bar}`);
+	logger.verbosity = v;
+}
+
+const deprecationNotice = `The changesets plugin is deprecated in favor of oneRepo’s built-in
+\`change\` commands. https://onerepo.tools/core/changes/`;
