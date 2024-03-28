867f066932b7a133b1ecd8402301a97c8e7de298
Paul Armstrong <paularmstrong@users.noreply.github.com>
2024-03-28 07:56:10 -0700

    docs: fix generate templatesDir (#672)
    Fixes #671

---
+++ modules/onerepo/.changes/001-small-steaks-sin.md
@@ -0,0 +1,5 @@
+---
+type: patch
+---
+
+Clarify `generate` command option is `templateDir` and not `templatesDir`, internally as well as in all help output & documentation.
--- modules/onerepo/src/core/generate/index.ts
+++ modules/onerepo/src/core/generate/index.ts
@@ -5,7 +5,7 @@ import * as cmd from './generate';
 export const generate: Plugin = function generate(opts) {
 	if (path.isAbsolute(opts.templateDir)) {
 		throw new Error(
-			'Invalid path specified for `core.generate.templatesDir`. Path must be relative to the repository root, like "./config/templates"',
+			'Invalid path specified for `templateDir`. Path must be relative to the repository root, like "./config/templates"',
 		);
 	}
 	const resolvedDir = path.resolve(process.env.ONEREPO_ROOT!, opts.templateDir ?? '');
@@ -20,7 +20,7 @@ export const generate: Plugin = function generate(opts) {
 				(yargs) => {
 					const y = builder(yargs)
 						.usage(`$0 ${Array.isArray(command) ? command[0] : command} [options...]`)
-						.default('templates-dir', resolvedDir)
+						.default('template-dir', resolvedDir)
 						.epilogue(
 							`To create new templates add a new folder to ${path.relative(
 								process.env.ONEREPO_ROOT!,
