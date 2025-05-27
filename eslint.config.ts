04a2bf5704f55046e29106e03273060e6423b7ae
Paul Armstrong <me@paularmstrong.dev>
2025-05-27 07:57:25 -0700

    fix: clean up some errors, ensure eslint syncing

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,14 +1,15 @@
 import tseslint from 'typescript-eslint';
 import { rootConfig } from '@internal/eslint-plugin';
 
+// start-synced-imports
+import docs from "@onerepo/docs/eslint.config";
+// end-synced-imports
+
 export default tseslint.config(
-	{
-		ignores: [
-			'**/dist',
-			'**/.yarn',
-			'*.tsbuildinfo',
-			/* TODO: sentinel for ignores */
-		],
-	},
+	{ ignores: ['**/dist', '**/.yarn', '*.tsbuildinfo'] },
 	rootConfig,
+	// start-synced-workspaces
+	{ files: ['./docs/**'], extends: [...docs] },
+
+	// end-synced-workspaces
 );
