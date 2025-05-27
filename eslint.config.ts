aec873cae4d201099684edd4b1cc1c28d8d74f66
Paul Armstrong <me@paularmstrong.dev>
2025-05-27 08:22:08 -0700

    feat(plugin-eslint): sync ignores

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,11 +2,20 @@ import tseslint from 'typescript-eslint';
 import { rootConfig } from '@internal/eslint-plugin';
 
 // start-synced-imports
-import docs from "@onerepo/docs/eslint.config";
+import docs from '@onerepo/docs/eslint.config';
 // end-synced-imports
 
 export default tseslint.config(
-	{ ignores: ['**/dist', '**/.yarn', '*.tsbuildinfo'] },
+	{
+		ignores: [
+			'**/dist',
+			'**/.yarn',
+			'*.tsbuildinfo',
+			// start-synced-ignores
+			'docs/.astro/**',
+			// end-synced-ignores
+		],
+	},
 	rootConfig,
 	// start-synced-workspaces
 	{ files: ['./docs/**'], extends: [...docs] },
