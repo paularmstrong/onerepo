b2bc83de0ff447912007f56f79f2090c4fb21fa9
Paul Armstrong <me@paularmstrong.dev>
2025-05-28 09:56:25 -0700

    refactor: add an eslint config wrapper

--- docs/eslint.config.ts
+++ docs/eslint.config.ts
@@ -1,10 +1,14 @@
 import tseslint from 'typescript-eslint';
 import pluginAstro from 'eslint-plugin-astro';
 
-export default tseslint.config({
-	ignores: ['.astro/**'],
-	extends: [...pluginAstro.configs.recommended],
-	rules: {
-		'import/no-unresolved': ['error', { ignore: ['astro:*'] }],
+export default tseslint.config(
+	{
+		ignores: ['.astro/**'],
 	},
-});
+	...pluginAstro.configs.recommended,
+	{
+		rules: {
+			'import/no-unresolved': ['error', { ignore: ['astro:*'] }],
+		},
+	},
+);
--- eslint.config.ts
+++ eslint.config.ts
@@ -1,25 +1,12 @@
 import tseslint from 'typescript-eslint';
 import { rootConfig } from '@internal/eslint-plugin';
+import onerepoEslint from '@onerepo/plugin-eslint/config';
 
-// start-synced-imports
-import docs from '@onerepo/docs/eslint.config';
-// end-synced-imports
-
-export default tseslint.config(
-	{
-		ignores: [
-			'**/dist',
-			'**/fixtures',
-			'**/.yarn',
-			'*.tsbuildinfo',
-			// start-synced-ignores
-			'docs/.astro/**',
-			// end-synced-ignores
-		],
-	},
-	rootConfig,
-	// start-synced-workspaces
-	{ files: ['./docs/**'], extends: [...docs] },
-
-	// end-synced-workspaces
+export default onerepoEslint(
+	tseslint.config(
+		{
+			ignores: ['**/dist', '**/fixtures', '**/.yarn', '*.tsbuildinfo'],
+		},
+		rootConfig,
+	),
 );
