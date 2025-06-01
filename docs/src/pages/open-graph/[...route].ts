154c791e4a349f570ef4b0f05f2f84ac34a98982
Paul Armstrong <me@paularmstrong.dev>
2025-06-01 09:47:33 -0700

    chore(docs): update astro/starlight

--- docs/src/pages/open-graph/[...route].ts
+++ docs/src/pages/open-graph/[...route].ts
@@ -2,9 +2,7 @@ import { getCollection } from 'astro:content';
 import { OGImageRoute } from 'astro-og-canvas';
 
 const collectionEntries = await getCollection('docs');
-const pages = Object.fromEntries(
-	collectionEntries.map(({ slug, data }: { slug: string; data: unknown }) => [slug, data]),
-);
+const pages = Object.fromEntries(collectionEntries.map(({ id, data }: { id: string; data: unknown }) => [id, data]));
 
 export const { getStaticPaths, GET } = OGImageRoute({
 	param: 'route',
